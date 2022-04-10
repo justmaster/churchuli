cf318a2e828b957d21a68a996898511a9dd96601
Tornike <t.j@Tornikes-MacBook-Air.local>
2022-04-10 13:18:40 +0400

    optimization for smaller older phones

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -26,17 +26,17 @@ const Navbar = () => {
     </div>
 
     <div className='app__navbar-smallscreen'>
-      <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => setToggleMenu(true)} />
+      <GiHamburgerMenu style={{cursor: "pointer"}}color="#fff" fontSize={27} onClick={() => setToggleMenu(true)} />
 
       {toggleMenu && (
       <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
         <MdOutlineMenu fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)}/>
         <ul className='app__navbar-smallscreen_links'>
-          <li className="p__opensans"><a href="#home">Home</a></li>
-          <li className="p__opensans"><a href="#about">About</a></li>
-          <li className="p__opensans"><a href="#menu">Menu</a></li>
-          <li className="p__opensans"><a href="#awards">Reviews</a></li>
-          <li className="p__opensans"><a href="#contact">Contact</a></li>
+          <li onClick={() => setToggleMenu(false)} className="p__opensans"><a href="#home">Home</a></li>
+          <li onClick={() => setToggleMenu(false)} className="p__opensans"><a href="#about">About</a></li>
+          <li onClick={() => setToggleMenu(false)} className="p__opensans"><a href="#menu">Menu</a></li>
+          <li onClick={() => setToggleMenu(false)} className="p__opensans"><a href="#awards">Reviews</a></li>
+          <li onClick={() => setToggleMenu(false)} className="p__opensans"><a href="#contact">Contact</a></li>
         </ul>
       </div>
       )}
